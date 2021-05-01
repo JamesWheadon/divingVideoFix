c17f00970214a3b8726b17cd678ea464a4b318e7
James Wheadon <james.wheadon@yahoo.com>
2021-05-01 15:19:30 +0100

    normal distribution, mean and std for colours

--- imageFix.py
+++ imageFix.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as stats
+
 im = Image.open('TurtleImage.jpg')
 width, height = im.size
+pixels = width * height
 origPixelMap = im.load()
 red = []
 green = []
@@ -16,7 +19,20 @@ for x in range(width):
 uniqueRed, countsRed = np.unique(red, return_counts=True)
 uniqueGreen, countsGreen = np.unique(green, return_counts=True)
 uniqueBlue, countsBlue = np.unique(blue, return_counts=True)
-plt.plot(uniqueRed, countsRed, color='red')
-plt.plot(uniqueGreen, countsGreen, color='green')
-plt.plot(uniqueBlue, countsBlue, color='blue')
+redMean = np.mean(red)
+redSTD = np.std(red)
+greenMean = np.mean(green)
+blueSTD = np.std(blue)
+blueMean = np.mean(blue)
+greenSTD = np.std(green)
+print(redMean, redSTD)
+print(greenMean, greenSTD)
+print(blueMean, blueSTD)
+plt.plot(uniqueRed, countsRed / pixels, color='red')
+plt.plot(uniqueGreen, countsGreen / pixels, color='green')
+plt.plot(uniqueBlue, countsBlue / pixels, color='blue')
+mu, sigma = 127, 30
+x = np.linspace(mu - 3*sigma, mu + 3*sigma, 1000)
+y = stats.norm.pdf(x, mu, sigma)
+plt.plot(x, y, color='black')
 plt.show()
